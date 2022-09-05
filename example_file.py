fb5458de8d260f4c56c12b2354199beb9af5b6f5
G-Licitra <gianni.licitra7@gmail.com>
2022-09-05 23:01:08 +0200

    [pre-commit] add

---
+++ example_file.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+x = {"a": 37, "b": 42, "c": True}
+if x["c"] is not None and x["a"] > 0 or x["b"] <= 214:
+    z = "hello " + "world"
+else:
+    world = "world"
+    a = "hello {}".format(world)
+    f = rf"hello {world}"
+
+
+class Foo(object):
+    def f(self):
+        return 37 * -2
+
+    def g(self, x, y=42):
+        return y
+
+
+# fmt: off
+custom_formatting = [
+    0,  1,  2,
+    3,  4,  5,
+    6,  7,  8,
+]
